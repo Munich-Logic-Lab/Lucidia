5caf5b2bdd0c2835cc6a1348c3e81551d4b5564e
Christoph Waffler <waffler.christoph@gmail.com>
2025-04-26 21:02:06 +0200

    chore: add double arrow icon

---
+++ src/components/icons/generated/DoubleArrow.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+
+const SvgDoubleArrow = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16.299}
+    height={18.022}
+    {...props}
+  >
+    <g data-name="Component 2 \u2013 1">
+      <g
+        fill="none"
+        stroke="#707070"
+        strokeLinecap="round"
+        strokeWidth={3}
+        data-name="Group 19"
+      >
+        <g data-name="Group 17">
+          <path d="M14.179 2.12 7.795 9.011" data-name="Line 2" />
+          <path d="M14.179 15.902 7.795 9.011" data-name="Line 3" />
+        </g>
+        <g data-name="Group 20">
+          <path d="M8.504 2.12 2.12 9.011" data-name="Line 2" />
+          <path d="M8.504 15.902 2.12 9.011" data-name="Line 3" />
+        </g>
+      </g>
+    </g>
+  </svg>
+);
+export default SvgDoubleArrow;
