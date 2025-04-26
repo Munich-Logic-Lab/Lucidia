6e635f72a46588ae452821f7793ed94f55039f0f
Christoph Waffler <waffler.christoph@gmail.com>
2025-04-27 00:37:04 +0200

    chore: footer

--- src/components/section/Footer.tsx
+++ src/components/section/Footer.tsx
@@ -1,9 +1,13 @@
 export default function Footer() {
   return (
-    <footer className="border-t border-zinc-100 py-8">
+    <footer className="border-t border-zinc-100 py-4">
       <div className="container mx-auto text-center">
-        <div className="text-sm text-gray-500">
-          © {new Date().getFullYear()} Lucidia
+        <div className="flex items-center justify-center gap-1 text-sm text-gray-500">
+          <span>© {new Date().getFullYear()} Lucidia</span>
+          <span className="mx-2">|</span>
+          <span className="flex items-center">
+            Made with 🤍 during the TUM.ai Makeathon in Munich
+          </span>
         </div>
       </div>
     </footer>
