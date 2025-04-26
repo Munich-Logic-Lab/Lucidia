8ad54eb8f8ac6586d825f47eccc634d555375f29
Christoph Waffler <waffler.christoph@gmail.com>
2025-04-26 21:22:41 +0200

    fix: logo design

--- src/components/custom/logo.tsx
+++ src/components/custom/logo.tsx
@@ -3,17 +3,18 @@ import Image from "next/image";
 export default function Logo({ className }: { className?: string }) {
   return (
     <div className={`flex items-center gap-2 ${className}`}>
-      <div className="relative h-6 w-6">
+      <div className="relative flex items-center">
         <Image
           src="/icons/logo.svg"
           alt="Lucidia Logo"
-          fill
           sizes="100%"
           className="object-contain"
           priority
+          height={45}
+          width={45}
         />
       </div>
-      <span className="text-lg font-medium tracking-wider text-[#394149] uppercase">
+      <span className="flex items-center text-[18px] leading-[23px] font-bold tracking-[1.08px] uppercase">
         Lucidia
       </span>
     </div>
