e375e919449fefc8e6051e9576c940dce6ce6ad7
Christoph Waffler <waffler.christoph@gmail.com>
2025-04-26 17:30:51 +0200

    chore: change linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -40,6 +40,12 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-unsafe-assignment": "warn",
       "@typescript-eslint/no-unsafe-call": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "@typescript-eslint/no-unsafe-return": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/prefer-nullish-coalescing": "warn",
       "@typescript-eslint/array-type": "off",
       "@typescript-eslint/consistent-type-definitions": "off",
       "@typescript-eslint/consistent-type-imports": [
